9c260c96da22e176561c5372de6f0f1618c46a96
Filip Grabovac <filip.grabovac@fer.unizg.hr>
2025-05-10 20:51:34 +0200

    add proper definition for google redirect uri

--- portiq/views.py
+++ portiq/views.py
@@ -1,8 +1,9 @@
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from dj_rest_auth.registration.views import SocialLoginView
+from portiq.settings import env
 
 class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
-    callback_url = "http://localhost:8000/auth/google/callback/"
+    callback_url = env("GOOGLE_REDIRECT_URI")
     client_class = OAuth2Client
